2291f468bd29a73e1e825d929815137b839a6bed
Dazzler123 <dasinduhewagamage@gmail.com>
2023-07-18 09:43:47 +0530

    Introduced Customer, Item and order routes

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const url = 'mongodb://localhost/GDSE';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-mongoose.connect(url, {useNewUrlParser:true});
-
-const conn = mongoose.connection;
-conn.on('open', function () {
-    console.log("!=== Connected ===!")
+//middleware
+app.use((req,res,next) => {
+    console.log(req.path,req.method);
+    next();
 })
 
 //routes
-const studentRouter = require('./routes/StudentRoutes');
-app.use('/student', studentRouter);
+app.use('api/v1/customer', require('./routes/CustomerRoutesController'));
+app.use('api/v1/item', require('./routes/ItemRoutesController'));
+app.use('api/v1/order', require('./routes/OrderRoutesController'));
 
-app.listen(port, () => {
+app.get('/', (req, res) => {
+    res.json({mssg: "Welcome to the App"})
+    console.log("Welcome to Dazzler's POS System - Server");
+})
+
+app.listen(port , () => {
     console.log(`API is running on Port ${port}`);
 })
 
